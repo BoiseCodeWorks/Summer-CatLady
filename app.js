94fb233779a72c54383409b474f9f6fb4d3b7214
Mark Ohnsman <mark.ohnsman@gmail.com>
2019-05-09 10:40:14 -0600

    Dallin

--- app.js
+++ app.js
@@ -2,5 +2,13 @@
 let cat = {
     name: "mr snibbly",
     moods: ["happy", "scratchy", "bitey", "ran away"],
-    pets: 0
+    pets: 0,
+    images: ['/assets/happycat.jpg', '/assets/sratchcat.jpg', '/assets/biteycat.jpg', '/assets/ranaway.jpeg']
+}
+
+function pet() {
+    //add one to pets every time pet is clicked
+    cat.pets++
+    //draw cat
+    drawCat()
 }
